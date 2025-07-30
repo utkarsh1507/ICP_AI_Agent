6a945f2c4c96f4884a1b89bd9984aa6701d4b7ef
Sumit240803 <goyalsumit651@gmail.com>
2025-07-30 05:34:05 -0700

    agent configuration

--- src/ai_agent_icp_backend/src/agent_config.rs
+++ src/ai_agent_icp_backend/src/agent_config.rs
@@ -1,12 +1,12 @@
 
-use candid::CandidType;
+use candid::{CandidType, Principal};
 use serde::{Deserialize, Serialize};
-#[derive(Debug,Serialize,Deserialize,CandidType)]
+#[derive(Debug,Serialize,Deserialize,CandidType,Clone)]
 pub struct AgentConfig{
     pub agent_id : u64,
     pub name : String,
     pub description : String,
-    pub owner : String,
+    pub owner : Principal,
     pub schedule : Schedule,
     pub tasks : Vec<Task>,
     pub created_at : i64,
@@ -15,13 +15,13 @@ pub struct AgentConfig{
     pub next_run : Option<i64>,
 }
 
-#[derive(Debug, Serialize, Deserialize,CandidType)]
+#[derive(Debug, Serialize, Deserialize,CandidType,Clone)]
 #[serde(tag = "type", rename_all = "snake_case")]
 pub enum Schedule {
     Interval{interval_days : u64},
     Cron{expression : String}
 }
-#[derive(Debug, Serialize, Deserialize,CandidType) ]
+#[derive(Debug, Serialize, Deserialize,CandidType,Clone) ]
 pub struct Task{
     pub tool : String,
     pub params : String
