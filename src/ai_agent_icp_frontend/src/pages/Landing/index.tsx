6217925ee9b4be3c568ac32b114efec329d7c04a
Sumit240803 <goyalsumit651@gmail.com>
2025-08-18 04:41:38 -0700

    ui

--- src/ai_agent_icp_frontend/src/pages/Landing/index.tsx
+++ src/ai_agent_icp_frontend/src/pages/Landing/index.tsx
@@ -1,16 +1,19 @@
 
+
 import Features from '../../components/pages/Landing/Features'
 import Footer from '../../components/pages/Landing/Footer'
 import Header from '../../components/pages/Landing/Header'
 import Hero from '../../components/pages/Landing/Hero'
 
+
+
 const Landing = () => {
   return (
-    <div>
-        <Header/>
-        <Hero/>
-        <Features/>
-        <Footer/>
+  <div >
+    <Header/>
+    <Hero/>
+    <Features/>
+    <Footer/>
     </div>
   )
 }
