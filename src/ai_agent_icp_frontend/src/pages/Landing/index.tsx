85e77fd08a4e32d2ac257ca8d1ec3cce92c0d7b6
Sumit240803 <goyalsumit651@gmail.com>
2025-08-19 03:14:22 -0700

    ui changed

--- src/ai_agent_icp_frontend/src/pages/Landing/index.tsx
+++ src/ai_agent_icp_frontend/src/pages/Landing/index.tsx
@@ -1,9 +1,11 @@
 
 
+import { Canvas } from '@react-three/fiber'
 import Features from '../../components/pages/Landing/Features'
 import Footer from '../../components/pages/Landing/Footer'
 import Header from '../../components/pages/Landing/Header'
 import Hero from '../../components/pages/Landing/Hero'
+import WireframeMesh from '../../components/ui/WireMesh'
 
 
 
@@ -11,9 +13,24 @@ const Landing = () => {
   return (
   <div >
     <Header/>
-    <Hero/>
+
+    <Canvas 
+    camera={{position : [0,0,5]}}
+    style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          zIndex: -1, // push behind content
+        
+        }}
+    >
+    <WireframeMesh/>
+    </Canvas>
     <Features/>
-    <Footer/>
+  
+
     </div>
   )
 }
